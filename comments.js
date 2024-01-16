4db70dd92ab1fd5966e43317c7cc16a4480d3c69
Prasanna-VV <54997759+Prasanna-VV@users.noreply.github.com>
2024-01-16 13:11:49 +0000

    Copilot third commit updated

--- comments.js
+++ comments.js
@@ -1,33 +1,33 @@
-// Create a web server that can respond to requests for /comments.json
-// with a JSON representation of an array of comments
+// Create a web server that can receive a POST request containing a comment and username field.
+// Store the comments in an array in memory.
+// Add a GET request to the path /comments that returns the list of comments.
+// Test your web server using Postman or curl.
 
-var http = require('http');
-var fs = require('fs');
-var comments = require('./comments.json');
+// 1. Import the express library
+const express = require("express");
 
-var server = http.createServer(function(req, res) {
-	if (req.method === 'GET' && req.url === '/comments.json') {
-		res.setHeader('Content-Type', 'application/json');
-		res.end(JSON.stringify(comments));
-	} else if (req.method === 'GET' && req.url === '/') {
-		res.setHeader('Content-Type', 'text/html');
-		fs.createReadStream('./public/index.html').pipe(res);
-	} else if (req.method === 'GET' && req.url === '/app.js') {
-		res.setHeader('Content-Type', 'application/javascript');
-		fs.createReadStream('./public/app.js').pipe(res);
-	} else if (req.method === 'POST' && req.url === '/comments') {
-		var body = '';
-		req.on('data', function(data) {
-			body += data;
-		});
-		req.on('end', function() {
-			var comment = JSON.parse(body);
-			comments.push(comment);
-			res.setHeader('Content-Type', 'application/json');
-			res.end(JSON.stringify(comment));
-		});
-	}
+// 2. Create an instance of an express app
+const app = express();
+
+// 3. Define a port to run on
+const port = 3000;
+
+// 4. Define a route handler for the default home page
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-server.listen(8080);
-console.log('Listening on port 8080');
+// 5. Define a route handler for the /comments post request
+app.post("/comments", (req, res) => {
+  res.send("Post request received");
+});
+
+// 6. Define a route handler for the /comments get request
+app.get("/comments", (req, res) => {
+  res.send("Get request received");
+});
+
+// 7. Start the server listening on the port
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
